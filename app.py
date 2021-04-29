364afed71687c3d946f6b10db705e2f254222b65
Ha young, Kim <jeremiah91@naver.com>
2021-04-30 00:27:23 +0900

    variable in Python

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print ("Hello World")
+# javaScript 처럼 hoisting 기능은 없음, 변수를 사용하고싶으면 반드시 변수를 맨위에 설정해야함
+# print ("Hello" + name)
+
+name = "Ha young"
+print (name)
+
+
+print ("Ha Young"[0])
+print ("Ha Young"[0:2])
+# 공백도 출력됨
+print ("Ha Young"[0:3])
